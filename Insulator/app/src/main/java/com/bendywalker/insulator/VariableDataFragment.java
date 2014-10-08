b1b6942bb101d221f9f98e6fca26d775d768f4dc
Ben David Walker <benwalker92@me.com>
2014-10-08 22:42:18 +0100

    Added Calculate button to VariableDataFragment.

--- Insulator/app/src/main/java/com/bendywalker/insulator/VariableDataFragment.java
+++ Insulator/app/src/main/java/com/bendywalker/insulator/VariableDataFragment.java
@@ -5,12 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class VariableDataFragment extends Fragment {
     Card currentBloodGlucoseLevelCard, carbohydratesInMealCard;
     TextView suggestedInsulinDoseTextView;
+    Button calculateButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +26,19 @@ public class VariableDataFragment extends Fragment {
         currentBloodGlucoseLevelCard = (Card) view.findViewById(R.id.card_current_blood_glucose_level);
         carbohydratesInMealCard = (Card) view.findViewById(R.id.card_carbohydrates_in_meal);
         suggestedInsulinDoseTextView = (TextView) view.findViewById(R.id.display_suggested_dose);
+        calculateButton = (Button) view.findViewById(R.id.button_calculate);
+
+        calculateButton.setOnClickListener(new MyOnClickListener());
 
         return view;
+    }
+
+    public class MyOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Calculator calculator = new Calculator(currentBloodGlucoseLevelCard.getFloatFromEntry(), carbohydratesInMealCard.getFloatFromEntry(), getActivity());
+            suggestedInsulinDoseTextView.setText(String.valueOf(calculator.getCalculatedInsulinDose()));
+        }
     }
 }
