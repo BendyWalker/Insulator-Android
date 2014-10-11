a1b663735343048c63aa56a148b926c66d345751
Ben David Walker <benwalker92@me.com>
2014-10-12 00:50:02 +0100

    Made mmol selected by default during welcome activity.

--- Insulator/app/src/main/java/com/bendywalker/insulator/WelcomePage2Fragment.java
+++ Insulator/app/src/main/java/com/bendywalker/insulator/WelcomePage2Fragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 
 
@@ -15,6 +16,7 @@ public class WelcomePage2Fragment extends Fragment
 {
     RelativeLayout halfUnitsCard, bloodGlucoseUnitsCard, continueCard;
     Button continueButton;
+    RadioGroup bloodGlucoseUnitsRadioGroup;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -28,11 +30,16 @@ public class WelcomePage2Fragment extends Fragment
     {
         View view = inflater.inflate(R.layout.fragment_welcome_page_2, container, false);
 
-        continueButton = (Button) view.findViewById(R.id.button_continue);
         halfUnitsCard = (RelativeLayout) view.findViewById(R.id.card_half_units);
+
         bloodGlucoseUnitsCard = (RelativeLayout) view
                 .findViewById(R.id.card_blood_glucose_measurement);
+        bloodGlucoseUnitsRadioGroup = (RadioGroup) view
+                .findViewById(R.id.card_blood_glucose_measurement_radio_group);
+        bloodGlucoseUnitsRadioGroup.check(R.id.card_blood_glucose_measurement_radio_button_mmol);
+
         continueCard = (RelativeLayout) view.findViewById(R.id.card_continue);
+        continueButton = (Button) view.findViewById(R.id.button_continue);
         continueButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
