00c6beb31775e7b528ef7c122e2f4b369c67cb6d
Ben David Walker <benwalker92@me.com>
2015-01-15 17:20:17 +0000

    Started implementing OnPreferenceChangeListener.

--- Insulator/app/src/main/java/com/bendywalker/insulator/SettingsFragment.java
+++ Insulator/app/src/main/java/com/bendywalker/insulator/SettingsFragment.java
@@ -1,16 +1,28 @@
 package com.bendywalker.insulator;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 
-public class SettingsFragment extends PreferenceFragment
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener
 {
+    Preference bloodGlucoseMeasurementPreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+bloodGlucoseMeasurementPreference = findPreference(getActivity().getString(R.string.preference_blood_glucose_units));
+        bloodGlucoseMeasurementPreference.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue)
+    {
+        
+        return false;
     }
 }
