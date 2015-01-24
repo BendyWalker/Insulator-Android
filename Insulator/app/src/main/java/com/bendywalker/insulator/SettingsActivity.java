cacd274687a1c51a3e3c60f3002ce55a3d0ad473
Ben David Walker <benwalker92@me.com>
2015-01-24 23:28:02 +0000

    Add App Bar back to SettingsActivity

--- Insulator/app/src/main/java/com/bendywalker/insulator/SettingsActivity.java
+++ Insulator/app/src/main/java/com/bendywalker/insulator/SettingsActivity.java
@@ -2,12 +2,19 @@ package com.bendywalker.insulator;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 
 public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener
@@ -22,6 +29,33 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
     {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        LinearLayout bar;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
+            bar = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.preferences_toolbar, root, false);
+            root.addView(bar, 0);
+        } else {
+            ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+            ListView content = (ListView) root.getChildAt(0);
+
+            root.removeAllViews();
+
+            bar = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.preferences_toolbar, root, false);
+            root.addView(bar);
+
+            int height;
+            TypedValue tv = new TypedValue();
+            if (getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
+                height = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+            }else{
+                height = bar.getHeight();
+            }
+
+            content.setPadding(0, height, 0, 0);
+
+            root.addView(content);
+        }
 
         preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
