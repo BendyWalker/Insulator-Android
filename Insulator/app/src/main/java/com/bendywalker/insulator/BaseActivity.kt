b4631ee4bf8e8d18ab832eb5011be1fa6d83436d
Ben Walker <ben.walker@3squared.com>
2017-01-09 12:00:19 +0000

    Create skeleton DashboardActivity

--- Insulator/app/src/main/java/com/bendywalker/insulator/BaseActivity.kt
+++ Insulator/app/src/main/java/com/bendywalker/insulator/BaseActivity.kt
@@ -7,7 +7,10 @@ import android.os.Bundle
  * Created by Ben David Walker (bendywalker) on 09/01/2017.
  */
 
-class BaseActivity : AppCompatActivity() {
+/*
+Add any functions or properties here that are required in all activities.
+ */
+open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ Insulator/app/src/main/java/com/bendywalker/insulator/DashboardActivity.kt
@@ -0,0 +1,5 @@
+package com.bendywalker.insulator
+
+class DashboardActivity : BaseActivity() {
+
+}
