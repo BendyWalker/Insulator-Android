fc89a2feb5c6911fe6c0019571321108e980fa96
Ben Walker <ben.walker@3squared.com>
2017-01-09 09:33:07 +0000

    Create PreferenceManager

---
+++ Insulator/app/src/main/java/com/bendywalker/insulator/PreferenceManager.kt
@@ -0,0 +1,63 @@
+package com.bendywalker.insulator
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+
+/**
+ * Created by Ben David Walker (bendywalker) on 09/01/2017.
+ */
+
+class PreferenceManager(context: Context) {
+    private val versionCodeKey = context.getString(R.string.key_version_code)
+    private val firstRunKey = context.getString(R.string.key_is_first_run)
+    private val allowFloatingPointCarbohydratesKey = context.getString(R.string.key_allow_floating_point_carbohydrates)
+    private val bloodGlucoseUnitKey = context.getString(R.string.key_blood_glucose_unit)
+    private val carbohydrateFactorKey = context.getString(R.string.key_carbohydrate_factor)
+    private val correctiveFactorKey = context.getString(R.string.key_corrective_factor)
+    private val desiredBloodGlucoseKey = context.getString(R.string.key_desired_blood_glucose)
+
+    private val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    var versionCode: Long
+        get() = preferences.getLong(versionCodeKey, 0)
+        set(value) {
+            preferences.edit().putLong(versionCodeKey, value).apply()
+        }
+
+    var firstRun: Boolean
+        get() = preferences.getBoolean(firstRunKey, true)
+        set(value) {
+            preferences.edit().putBoolean(firstRunKey, value).apply()
+        }
+
+    var allowFloatingPointCarbohydrates: Boolean
+        get() = preferences.getBoolean(allowFloatingPointCarbohydratesKey, false)
+        set(value) {
+            preferences.edit().putBoolean(allowFloatingPointCarbohydratesKey, value).apply()
+        }
+
+    var bloodGlucoseUnit: BloodGlucoseUnit
+        get() = BloodGlucoseUnit.valueOf(preferences.getString(bloodGlucoseUnitKey, "MMOL"))
+        set(value) {
+            preferences.edit().putString(bloodGlucoseUnitKey, value.toString()).apply()
+        }
+
+    var carbohydrateFactor: Double
+        get() = preferences.getString(carbohydrateFactorKey, "0.0").toDouble()
+        set(value) {
+            preferences.edit().putString(carbohydrateFactorKey, value.toString()).apply()
+        }
+
+    var correctiveFactor: Double
+        get() = preferences.getString(correctiveFactorKey, "0.0").toDouble()
+        set(value) {
+            preferences.edit().putString(correctiveFactorKey, value.toString()).apply()
+        }
+
+    var desiredBloodGlucose: Double
+        get() = preferences.getString(desiredBloodGlucoseKey, "0.0").toDouble()
+        set(value) {
+            preferences.edit().putString(desiredBloodGlucoseKey, value.toString()).apply()
+        }
+}
