e67c46b0eeee80fcdc3b62bd55b0595063120a45
Ben David Walker <benwalker92@me.com>
2017-01-11 18:35:39 +0000

    Change view_card to use LinearLayouts

--- Insulator/app/src/main/java/com/bendywalker/insulator/Card.kt
+++ Insulator/app/src/main/java/com/bendywalker/insulator/Card.kt
@@ -5,16 +5,16 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 
 /**
  * Created by Ben David Walker (bendywalker) on 11/01/2017.
  */
-class Card(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : RelativeLayout(context, attrs, defStyleAttr, defStyleRes) {
+class Card(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0, 0)
     constructor(context: Context) : this(context, null, 0, 0)
-
 
 
     init {
