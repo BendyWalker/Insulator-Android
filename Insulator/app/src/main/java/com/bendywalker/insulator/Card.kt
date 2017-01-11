6f05521021275874b8ce75a4c647ac7106cc184b
Ben David Walker <benwalker92@me.com>
2017-01-11 18:12:59 +0000

    Create skeleton Card class

---
+++ Insulator/app/src/main/java/com/bendywalker/insulator/Card.kt
@@ -0,0 +1,24 @@
+package com.bendywalker.insulator
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+
+/**
+ * Created by Ben David Walker (bendywalker) on 11/01/2017.
+ */
+class Card(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : RelativeLayout(context, attrs, defStyleAttr, defStyleRes) {
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0, 0)
+    constructor(context: Context) : this(context, null, 0, 0)
+
+
+
+    init {
+        val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        layoutInflater.inflate(0, this)
+    }
+}
