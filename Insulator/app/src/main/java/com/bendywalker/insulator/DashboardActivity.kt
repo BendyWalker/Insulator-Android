6b06994e57418217d353467a489b28a4b842b9e4
Ben Walker <ben.walker@3squared.com>
2017-01-12 13:46:00 +0000

    Remove placeholder/test dashboard code

--- Insulator/app/src/main/java/com/bendywalker/insulator/DashboardActivity.kt
+++ Insulator/app/src/main/java/com/bendywalker/insulator/DashboardActivity.kt
@@ -12,10 +12,6 @@ import android.widget.TextView
 
 class DashboardActivity : BaseActivity() {
     val persistedValues: PersistedValues by lazy { PersistedValues(this) }
-    val currentBloodGlucoseEditText: EditText by lazy { findViewById(R.id.editText_dashboard_currentBloodGlucose) as EditText }
-    val carbohydratesInMealEditText: EditText by lazy { findViewById(R.id.editText_dashboard_carbohydratesInMeal) as EditText }
-    val totalDoseTextView: TextView by lazy { findViewById(R.id.textView_dashboard_totalDose) as TextView }
-    val calculateButton: Button by lazy { findViewById(R.id.button_dashboard_calculate) as Button }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,27 +26,12 @@ class DashboardActivity : BaseActivity() {
             exception.printStackTrace()
         }
 
-        persistedValues.carbohydrateFactor = 10.0
-        persistedValues.correctiveFactor = 2.0
-        persistedValues.allowFloatingPointCarbohydrates = false
-        persistedValues.desiredBloodGlucose = 6.5
-        persistedValues.firstRun = false
-        persistedValues.bloodGlucoseUnit = BloodGlucoseUnit.MMOL
-
         if (persistedValues.firstRun) {
             // TODO: Start welcome activity
             finish()
         } else {
             // TODO: Setup activity layout
             setContentView(R.layout.activity_dashboard)
-
-            val calculator = Calculator(persistedValues = persistedValues)
-
-            calculateButton.setOnClickListener {
-                calculator.currentBloodGlucose = currentBloodGlucoseEditText.text.toString().toDouble()
-                calculator.carbohydratesInMeal = carbohydratesInMealEditText.text.toString().toDouble()
-                totalDoseTextView.text = calculator.totalDose.toString()
-            }
         }
     }
 }
